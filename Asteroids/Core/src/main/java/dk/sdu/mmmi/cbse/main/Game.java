c98716b5216c6e29a5d420fd849c966d4f7d3dd7
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-05-16 02:07:44 +0200

    :recycle: Use GetBeansOfType

--- Asteroids/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
+++ Asteroids/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -5,12 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import dk.sdu.mmmi.cbse.asteroidsplittingsystem.AsteroidSplitter;
-import dk.sdu.mmmi.cbse.asteroidsystem.AsteroidControlSystem;
-import dk.sdu.mmmi.cbse.asteroidsystem.AsteroidPlugin;
-import dk.sdu.mmmi.cbse.bulletsystem.BulletControlSystem;
-import dk.sdu.mmmi.cbse.bulletsystem.BulletPlugin;
-import dk.sdu.mmmi.cbse.collisiondetectionsystem.CollisionDetection;
 import dk.sdu.mmmi.cbse.common.IShapeRender;
 import dk.sdu.mmmi.cbse.common.MyShapeRender;
 import dk.sdu.mmmi.cbse.common.data.Entity;
@@ -19,16 +13,10 @@ import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-import dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
-import dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
-import dk.sdu.mmmi.cbse.lifeprocessersystem.LifeProcesser;
 import dk.sdu.mmmi.cbse.managers.GameInputProcessor;
-import dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
-import dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class Game implements ApplicationListener {
@@ -107,28 +95,14 @@ public class Game implements ApplicationListener {
     }
 
     private Collection<? extends IGamePluginService> getPluginServices() {
-        Collection<IGamePluginService> plugins = new ArrayList<>();
-        plugins.add(appContext.getBean("AsteroidPlugin", AsteroidPlugin.class));
-        plugins.add(appContext.getBean("EnemyPlugin", EnemyPlugin.class));
-        plugins.add(appContext.getBean("PlayerPlugin", PlayerPlugin.class));
-        plugins.add(appContext.getBean("BulletPlugin", BulletPlugin.class));
-        return plugins;
+        return appContext.getBeansOfType(IGamePluginService.class).values();
     }
 
     private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
-        Collection<IEntityProcessingService> services = new ArrayList<>();
-        services.add(appContext.getBean("AsteroidControlSystem", AsteroidControlSystem.class));
-        services.add(appContext.getBean("EnemyControlSystem", EnemyControlSystem.class));
-        services.add(appContext.getBean("PlayerControlSystem", PlayerControlSystem.class));
-        services.add(appContext.getBean("BulletControlSystem", BulletControlSystem.class));
-        return services;
+        return appContext.getBeansOfType(IEntityProcessingService.class).values();
     }
 
     private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
-        Collection<IPostEntityProcessingService> services = new ArrayList<>();
-        services.add(appContext.getBean("AsteroidSplitter", AsteroidSplitter.class));
-        services.add(appContext.getBean("CollisionDetection", CollisionDetection.class));
-        services.add(appContext.getBean("LifeProcesser", LifeProcesser.class));
-        return services;
+        return appContext.getBeansOfType(IPostEntityProcessingService.class).values();
     }
 }
